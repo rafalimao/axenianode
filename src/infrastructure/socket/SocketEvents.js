1fe0b50925519013acb2377641c7b451e07eefca
Rafael Lima da Costa <rafael.costa@aqbank.com.br>
2025-08-03 21:16:31 -0300

    puppeteer

--- src/infrastructure/socket/SocketEvents.js
+++ src/infrastructure/socket/SocketEvents.js
@@ -40,7 +40,6 @@ function setupSocketEvents(io) {
             const client = new Client({
                 authStrategy: new LocalAuth({ clientId: userId }),
                 puppeteer: {
-                    executablePath: '/opt/chromium/chromium1496088/chrome',
                     headless: true,
                     args: [
                         '--no-sandbox',
